7e024f1b3760bbda5410ed47a3a1df0fa0596f18
Vasu Mahesh <vasu.mahesh@yahoo.com>
2016-01-05 18:17:57 +0530

    Fixed - TemplateURL

--- src/growlDirective.js
+++ src/growlDirective.js
@@ -4,7 +4,7 @@ angular.module("angular-growl").directive("growl", [
 
     return {
       restrict: 'A',
-      templateUrl: 'templates/growl/growl.html',
+      templateUrl: 'app/dir/growl.tpl.html',
       replace: false,
       scope: {
         reference: '@',
@@ -77,19 +77,3 @@ angular.module("angular-growl").directive("growl", [
     };
   }
 ]);
-
-angular.module("angular-growl").run(['$templateCache', function ($templateCache) {
-  "use strict";
-  if ($templateCache.get('templates/growl/growl.html') === undefined) {
-    $templateCache.put("templates/growl/growl.html",
-      '<div class="growl-container" ng-class="wrapperClasses()">' +
-      '<div class="growl-item alert" ng-repeat="message in growlMessages.directives[referenceId].messages" ng-class="alertClasses(message)" ng-click="stopTimeoutClose(message)">' +
-      '<button type="button" class="close" data-dismiss="alert" aria-hidden="true" ng-click="growlMessages.deleteMessage(message)" ng-show="!message.disableCloseButton">&times;</button>' +
-      '<button type="button" class="close" aria-hidden="true" ng-show="showCountDown(message)">{{message.countdown}}</button>' +
-      '<h4 class="growl-title" ng-show="message.title" ng-bind="message.title"></h4>' +
-      '<div class="growl-message" ng-bind-html="message.text"></div>' +
-      '</div>' +
-      '</div>'
-    );
-  }
-}]);
